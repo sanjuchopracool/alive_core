fa3285fc8ccf165a4ac25c330208b6c7ece804c7
Sanjay <sanjuchopracool@gmail.com>
2024-06-02 01:23:57 +0530

    added time to check performance when moving to newer version of library

--- src/core_utility/main.cpp
+++ src/core_utility/main.cpp
@@ -2,6 +2,7 @@
 #include <core/inae_version.h>
 #include <core/initializer.h>
 #include <core/model/composition.h>
+#include <core/profiler.h>
 #include <core/serializer/inae_parser.h>
 #include <cxxopts.hpp>
 #include <filesystem>
@@ -9,24 +10,26 @@
 #include <fstream>
 #include <iostream>
 
-
 const std::string k_resources_key{"resources"};
 const std::string k_file_key{"file"};
 const std::string k_scale_key{"scale"};
 const std::string k_output_dir_key{"outputdir"};
 const std::string k_output_file_key{"outputfile"};
 
-void return_invalid_arg(std::string_view message) {
+void return_invalid_arg(std::string_view message)
+{
     std::cout << message << std::endl;
     exit(-1);
 }
 
-void return_runtime_error(std::string_view message) {
+void return_runtime_error(std::string_view message)
+{
     std::cout << message << std::endl;
     exit(1);
 }
 
-void return_invalid_arg(std::string_view message, std::string_view help) {
+void return_invalid_arg(std::string_view message, std::string_view help)
+{
     std::cout << message << std::endl;
     std::cout << help << std::endl;
     exit(-1);
@@ -36,54 +39,64 @@ int main(int argc, char *argv[])
 {
     cxxopts::Options options("core_utility", "Utility to render inae projects");
 
-    options.add_options()
-        ("r,resources", "Resources directory containing inae_resources", cxxopts::value<std::string>())
-        ("s,scale", "Scale", cxxopts::value<float>()->default_value("1.0"))
-        ("f,file", "Input json file absolute path", cxxopts::value<std::string>())
-        ("w,outputfile", "Output file path for exported webp file", cxxopts::value<std::string>())
-        ("o,outputdir", "Output directory for exported png", cxxopts::value<std::string>())
-        ("h,help", "Print usage");
+    options
+        .add_options()("r,resources",
+                       "Resources directory containing inae_resources",
+                       cxxopts::value<std::string>())("s,scale",
+                                                      "Scale",
+                                                      cxxopts::value<float>()->default_value(
+                                                          "1.0"))("f,file",
+                                                                  "Input json file absolute path",
+                                                                  cxxopts::value<std::string>())(
+            "w,outputfile",
+            "Output file path for exported webp file",
+            cxxopts::value<std::string>())("o,outputdir",
+                                           "Output directory for exported png",
+                                           cxxopts::value<std::string>())("h,help", "Print usage");
 
     cxxopts::ParseResult result;
     try {
         result = options.parse(argc, argv);
-    } catch(const cxxopts::exceptions::exception& e) {
+    } catch (const cxxopts::exceptions::exception &e) {
         return_invalid_arg(e.what(), options.help());
-    } catch(...) {
+    } catch (...) {
         return_invalid_arg(options.help());
     }
 
-    if (result.count("help"))
-    {
+    if (result.count("help")) {
         return_invalid_arg(options.help());
     }
 
     std::string input_file_path;
-    if (result.count(k_file_key) == 0)
-    {
+    if (result.count(k_file_key) == 0) {
         return_invalid_arg("missing file argument\n", options.help());
     } else {
         input_file_path = result[k_file_key].as<std::string>();
-        if (!std::filesystem::exists(input_file_path) || !std::filesystem::is_regular_file(input_file_path)) {
-         return_invalid_arg(fmt::format("input file {} does not exist\n", input_file_path), options.help());
+        if (!std::filesystem::exists(input_file_path)
+            || !std::filesystem::is_regular_file(input_file_path)) {
+            return_invalid_arg(fmt::format("input file {} does not exist\n", input_file_path),
+                               options.help());
         }
     }
 
     std::string output_dir_path;
-    if (result.count(k_output_dir_key))
-    {
+    if (result.count(k_output_dir_key)) {
         output_dir_path = result[k_output_dir_key].as<std::string>();
-        if (!std::filesystem::exists(output_dir_path) || !std::filesystem::is_directory(output_dir_path)) {
-            return_invalid_arg(fmt::format("output directory {} does not exist\n", output_dir_path), options.help());
+        if (!std::filesystem::exists(output_dir_path)
+            || !std::filesystem::is_directory(output_dir_path)) {
+            return_invalid_arg(fmt::format("output directory {} does not exist\n", output_dir_path),
+                               options.help());
         }
     }
 
     std::filesystem::path output_file_path;
-    if (result.count(k_output_file_key))
-    {
-         output_file_path = result[k_output_file_key].as<std::string>();
-        if (!std::filesystem::exists(output_file_path.parent_path()) || !std::filesystem::is_directory(output_file_path.parent_path())) {
-            return_invalid_arg(fmt::format("output file directory does not exist for path {} \n", output_file_path.string()), options.help());
+    if (result.count(k_output_file_key)) {
+        output_file_path = result[k_output_file_key].as<std::string>();
+        if (!std::filesystem::exists(output_file_path.parent_path())
+            || !std::filesystem::is_directory(output_file_path.parent_path())) {
+            return_invalid_arg(fmt::format("output file directory does not exist for path {} \n",
+                                           output_file_path.string()),
+                               options.help());
         }
     }
 
@@ -102,7 +115,9 @@ int main(int argc, char *argv[])
     }
 
     if (scale < 0.1) {
-        return_invalid_arg(fmt::format("invalid scale {}, scale should be greater than 0.1\n", scale), options.help());
+        return_invalid_arg(fmt::format("invalid scale {}, scale should be greater than 0.1\n",
+                                       scale),
+                           options.help());
     }
 
     if (resources_dir.empty()) {
@@ -121,14 +136,13 @@ int main(int argc, char *argv[])
     inae::core::parser::ParserMessages messages;
     auto comp = inae::core::parser::parse_inae(buffer.c_str(), setting, messages);
     if (comp) {
+        inae::AutoProfiler p("Time Taken");
         inae::exporter::Exporter image_exporter = inae::exporter::Exporter(comp.get());
-        if (!output_dir_path.empty())
-        {
+        if (!output_dir_path.empty()) {
             image_exporter.export_as(output_dir_path);
         }
 
-        if (!output_file_path.empty())
-        {
+        if (!output_file_path.empty()) {
             image_exporter.export_as(output_file_path.string(), inae::exporter::Exporter::e_WebM);
         }
 
