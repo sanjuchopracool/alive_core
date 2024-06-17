4aa5f025bad25301ce2f1a37aa6fc9f8a1af2b9b
Sanjay <sanjuchopracool@gmail.com>
2024-06-17 19:41:51 +0530

    adding fixes

--- src/core/script/expression_engine.cpp
+++ src/core/script/expression_engine.cpp
@@ -1,9 +1,11 @@
 #include "expression_engine.h"
+#include <Magnum/Math/Vector3.h>
 #include <core/composition/animation_nodes/transformation_node.h>
 #include <core/composition/composition_node.h>
 #include <core/composition/solid_layer_node.h>
 #include <core/composition/text_layer_node.h>
 #include <core/model/text_primitives/text_document_property.h>
+
 namespace inae::script {
 
 void register_types(sol::state &lua)
@@ -13,36 +15,47 @@ void register_types(sol::state &lua)
     // Vec3D
     sol::usertype<Vec3D> vec3_typ = lua.new_usertype<Vec3D>("Vec3D", sol::constructors<Vec3D()>());
     vec3_typ["create"] = &create_vec3d;
-    vec3_typ["x"] = &Vec3D::x;
-    // vec3_typ["y"] = &Vec3D::y;
-    // vec3_typ["z"] = &Vec3D::z;
+    vec3_typ.set("x",
+                 sol::property([](Vec3D &type) { return type.x(); },
+                               [](Vec3D &type, float val) { type.x() = val; }));
+    vec3_typ.set("y",
+                 sol::property([](Vec3D &type) { return type.y(); },
+                               [](Vec3D &type, float val) { type.y() = val; }));
+
+    vec3_typ.set("z",
+                 sol::property([](Vec3D &type) { return type.z(); },
+                               [](Vec3D &type, float val) { type.z() = val; }));
 
     // Vec2D
-    // sol::usertype<Vec2D> vec2_typ = lua.new_usertype<Vec2D>("Vec2D");
-    // vec2_typ["create"] = &create_vec2d;
-    // vec2_typ["x"] = &Vec2D::x;
-    // vec2_typ["y"] = &Vec2D::y;
+    sol::usertype<Vec2D> vec2_typ = lua.new_usertype<Vec2D>("Vec2D");
+    vec2_typ["create"] = &create_vec2d;
+    vec2_typ.set("x",
+                 sol::property([](Vec2D &type) { return type.x(); },
+                               [](Vec2D &type, float val) { type.x() = val; }));
+    vec2_typ.set("y",
+                 sol::property([](Vec2D &type) { return type.y(); },
+                               [](Vec2D &type, float val) { type.y() = val; }));
 
     // Rect
-    // sol::usertype<Rect> rect_typ = lua.new_usertype<Rect>("Rect");
-    // rect_typ["x"] = sol::readonly_property(&Rect::x);
-    // rect_typ["y"] = sol::readonly_property(&Rect::y);
-    // rect_typ["left"] = sol::readonly_property(&Rect::left);
-    // rect_typ["right"] = sol::readonly_property(&Rect::right);
-    // rect_typ["top"] = sol::readonly_property(&Rect::top);
-    // rect_typ["bottom"] = sol::readonly_property(&Rect::bottom);
-    // rect_typ["width"] = sol::readonly_property(&Rect::width);
-    // rect_typ["height"] = sol::readonly_property(&Rect::height);
-    // rect_typ["center"] = sol::readonly_property(&Rect::center);
-    // rect_typ["center_x"] = sol::readonly_property(&Rect::centerX);
-    // rect_typ["center_y"] = sol::readonly_property(&Rect::centerY);
+    sol::usertype<SkRect> sk_rect_typ = lua.new_usertype<SkRect>("SkRect");
+    sk_rect_typ["x"] = sol::readonly_property(&SkRect::x);
+    sk_rect_typ["y"] = sol::readonly_property(&SkRect::y);
+    sk_rect_typ["left"] = sol::readonly_property(&SkRect::left);
+    sk_rect_typ["right"] = sol::readonly_property(&SkRect::right);
+    sk_rect_typ["top"] = sol::readonly_property(&SkRect::top);
+    sk_rect_typ["bottom"] = sol::readonly_property(&SkRect::bottom);
+    sk_rect_typ["width"] = sol::readonly_property(&SkRect::width);
+    sk_rect_typ["height"] = sol::readonly_property(&SkRect::height);
+    sk_rect_typ["center"] = sol::readonly_property(&SkRect::center);
+    sk_rect_typ["center_x"] = sol::readonly_property(&SkRect::centerX);
+    sk_rect_typ["center_y"] = sol::readonly_property(&SkRect::centerY);
 
-    // // Point
-    // sol::usertype<Point> point_typ = lua.new_usertype<Point>("Point");
-    // point_typ["x"] = sol::readonly_property(&Point::x);
-    // point_typ["y"] = sol::readonly_property(&Point::y);
-    // point_typ["is_zero"] = sol::readonly_property(&Point::isZero);
-    // point_typ["create"] = &SkPoint::Make;
+    // Point
+    sol::usertype<Point> point_typ = lua.new_usertype<Point>("Point");
+    point_typ["x"] = sol::readonly_property([](Point &type) { return type.x(); });
+    point_typ["y"] = sol::readonly_property([](Point &type) { return type.y(); });
+    point_typ["is_zero"] = sol::readonly_property(&Point::isZero);
+    vec2_typ["create"] = &create_vec2d;
 
     // Text
     sol::usertype<core::TextDocumentProperty> text_type
--- src/core/script_sandbox/main.cpp
+++ src/core/script_sandbox/main.cpp
@@ -31,86 +31,109 @@ private:
     SkV3 m_d3;
 };
 
-template<typename T>
-struct Vector1
+// Small example class  similar to magnum vector2
+template<std::size_t size, class T>
+class MyVector
 {
-public:
-    T &x() { return m_x; }
-
-private:
-    T m_x;
+protected:
+    T _data[size];
 };
 
-using Vec1D = Vector1<float>;
+template<typename T>
+class MyVector2 : public MyVector<2, T>
+{
+public:
+    T &x() { return MyVector<2, T>::_data[0]; }
+    const T &x() const { return MyVector<2, T>::_data[0]; }
+    T &y() { return MyVector<2, T>::_data[1]; }
+    const T &y() const { return MyVector<2, T>::_data[1]; }
+};
 
 int main()
 {
     sol::state lua;
     lua.open_libraries(sol::lib::base, sol::lib::package);
-    sol::usertype<Vec1D> vec1_typ = lua.new_usertype<Vec1D>("Vec1D", sol::constructors<Vec1D()>());
-    vec1_typ["x"] = &Vec1D::x;
+    lua.script("print('bark bark bark!')");
 
-    // lua.script("print('bark bark bark!')");
-    // // make usertype metatable
-    // sol::usertype<Base> base_type
-    //     = lua.new_usertype<Base>("Base",
-    //                              // 3 constructors
-    //                              sol::constructors<Base(int, const std::string &)>());
-    // sol::usertype<SkV3> vec3_typ = lua.new_usertype<SkV3>("Vec3D",
-    //                                                       // 3 constructors
-    //                                                       sol::constructors<SkV3()>());
-    // vec3_typ["x"] = &SkV3::x;
-    // vec3_typ["y"] = &SkV3::y;
-    // vec3_typ["z"] = &SkV3::z;
+    using MyVec2D = MyVector2<float>;
+    MyVec2D my_vec;
 
-    // sol::usertype<SkV2> vec2_typ = lua.new_usertype<SkV2>("Vec2D");
-    // vec2_typ["x"] = &SkV2::x;
-    // vec2_typ["y"] = &SkV2::y;
+    // Gives compilation error
+    sol::usertype<MyVec2D> my_vec2_type = lua.new_usertype<MyVec2D>("MyVec2D",
+                                                                    sol::constructors<MyVec2D()>());
+    my_vec2_type.set("x",
+                     sol::property([](MyVec2D &type) { return type.x(); },
+                                   [](MyVec2D &type, float val) { type.x() = val; }));
+    my_vec2_type.set("y",
+                     sol::property([](MyVec2D &type) { return type.y(); },
+                                   [](MyVec2D &type, float val) { type.y() = val; }));
 
-    // base_type["data"] = sol::property(&Base::data, &Base::set_data);
-    // base_type["set_des"] = &Base::set_des;
-    // base_type["des"] = &Base::des;
-    // base_type["d3"] = &Base::d3;
-    // //    base_type["m_d3"] = &Base::m_d3;  // This does not work
-    // base_type.set("name", sol::readonly(&Base::name));
+    MyVec2D r_vec = lua.script("vec2 = MyVec2D.new()\n"
+                               "vec2.x = 5\n"
+                               "vec2.y = 7\n"
+                               "return vec2");
+    cout << "Vec2D : " << r_vec.x() << " " << r_vec.y() << endl;
 
-    // //    base_type["description"] = &Base::des;
-    // lua.script("base = Base.new(1, \"Hello\")\n"
-    //            "base.data = 12\n"
-    //            "b_name = base:name()");
+    // make usertype metatable
+    sol::usertype<Base> base_type
+        = lua.new_usertype<Base>("Base",
+                                 // 3 constructors
+                                 sol::constructors<Base(int, const std::string &)>());
+    sol::usertype<SkV3> vec3_typ = lua.new_usertype<SkV3>("Vec3D",
+                                                          // 3 constructors
+                                                          sol::constructors<SkV3()>());
+    vec3_typ["x"] = &SkV3::x;
+    vec3_typ["y"] = &SkV3::y;
+    vec3_typ["z"] = &SkV3::z;
 
-    // Base &base = lua["base"];
-    // const std::string &name = lua["b_name"];
-    // cout << base.data() << " " << name << endl;
+    sol::usertype<SkV2> vec2_typ = lua.new_usertype<SkV2>("Vec2D");
+    vec2_typ["x"] = &SkV2::x;
+    vec2_typ["y"] = &SkV2::y;
 
-    // /////////////////////////////////////////
-    // Base b2(43, "asdasd");
-    // lua["base2"] = std::ref(b2);
-    // lua.script("base2:set_des(\"BASE 2\")");
-    // cout << b2.des() << endl;
+    base_type["data"] = sol::property(&Base::data, &Base::set_data);
+    base_type["set_des"] = &Base::set_des;
+    base_type["des"] = &Base::des;
+    base_type["d3"] = &Base::d3;
+    //    base_type["m_d3"] = &Base::m_d3;  // This does not work
+    base_type.set("name", sol::readonly(&Base::name));
 
-    // SkV3 ret = lua.script("return base2:d3()");
-    // cout << ret.x << " " << ret.y << " " << ret.z << endl;
-    // b2.set_d3({123.1, 123.1, 123});
-    // ret = lua.script("return base2:d3()");
-    // cout << ret.x << " " << ret.y << " " << ret.z << endl;
-    // ret = lua.script("vec3 = Vec3D.new()\n"
-    //                  "vec3.x = 0\n"
-    //                  "vec3.y = 1\n"
-    //                  "vec3.z = 2\n"
-    //                  "return vec3");
-    // cout << ret.x << " " << ret.y << " " << ret.z << endl;
+    //    base_type["description"] = &Base::des;
+    lua.script("base = Base.new(1, \"Hello\")\n"
+               "base.data = 12\n"
+               "b_name = base:name()");
 
-    // SkV2 vec{123.23, 532.123};
-    // lua["obj2d"] = std::ref(vec);
-    // float x = lua.script("return obj2d.x");
-    // float y = lua.script("return obj2d.y");
-    // float result = lua.script("return (obj2d.y + obj2d.x)*0.5");
-    // cout << x << " " << y << " average = " << result << endl;
+    Base &base = lua["base"];
+    const std::string &name = lua["b_name"];
+    cout << base.data() << " " << name << endl;
 
-    // /////////////////////////////////////////
-    // const std::string &name2 = lua.script("base2:set_des(\"CHECK\")\n"
-    //                                       "return base2:des()");
-    // cout << name2 << endl;
+    /////////////////////////////////////////
+    Base b2(43, "asdasd");
+    lua["base2"] = std::ref(b2);
+    lua.script("base2:set_des(\"BASE 2\")");
+    cout << b2.des() << endl;
+
+    SkV3 ret = lua.script("return base2:d3()");
+    cout << ret.x << " " << ret.y << " " << ret.z << endl;
+    b2.set_d3({123.1, 123.1, 123});
+    ret = lua.script("return base2:d3()");
+    cout << ret.x << " " << ret.y << " " << ret.z << endl;
+    ret = lua.script("vec3 = Vec3D.new()\n"
+                     "vec3.x = 0\n"
+                     "vec3.y = 1\n"
+                     "vec3.z = 2\n"
+                     "return vec3");
+    cout << ret.x << " " << ret.y << " " << ret.z << endl;
+
+    SkV2 vec{123.23, 532.123};
+    lua["obj2d"] = std::ref(vec);
+    float x = lua.script("return obj2d.x");
+    float y = lua.script("return obj2d.y");
+    float result = lua.script("return (obj2d.y + obj2d.x)*0.5");
+    cout << x << " " << y << " average = " << result << endl;
+
+    /////////////////////////////////////////
+    const std::string &name2 = lua.script("base2:set_des(\"CHECK\")\n"
+                                          "return base2:des()");
+    cout << name2 << endl;
     return 0;
 }
